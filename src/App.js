47ed304f40a207687dac523aa5a9a7c72485da03
aditya5321 <82571426+aditya5321@users.noreply.github.com>
2024-01-01 02:21:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <Particles init={particlesinit} options={{ preset: 'fireworks' }} />;
       <div className="flex flex-col justify-center items-center min-h-screen gap-4">
         <span className="text-white text-4xl font-bold px-4 z-50">
-          <Typewriter words={newYearMessage} loop={false} cursorStyle={'_'} cursor typeSpeed={50} />
+          <Typewriter words={newYearMessage} loop={false} cursorStyle={'✍️'} cursor typeSpeed={50} />
         </span>
         <div className="z-50 text-white font-bold text-2xl">
         
